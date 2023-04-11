f95569679729bf06b54b854ff3464d41a68e4822
Carmen-Trillo <mctrillon@gmail.com>
2023-04-11 14:33:49 +0200

    Created legal text

--- src/pages/Cookies.jsx
+++ src/pages/Cookies.jsx
@@ -2,7 +2,61 @@ const Cookies = () => {
 
     return (
         <>
-           
+           <div className="legal">
+            <h1>Política de Cookies</h1>
+            <h2>¿Qué son las cookies?</h2>
+            <p>En inglés, el término "cookie" significa galleta, pero en el ámbito de la navegación web, una "cookie" es algo completamente distinto. Cuando accede a nuestro Sitio Web, en el navegador de su dispositivo se almacena una pequeña cantidad de texto que se denomina "cookie". Este texto contiene información variada sobre su navegación, hábitos, preferencias, personalizaciones de contenidos, etc...</p>
+            <p>Existen otras tecnologías que funcionan de manera similar y que también se usan para recopilar datos sobre tu actividad de navegación. Llamaremos "cookies" a todas estas tecnologías en su conjunto.</p>   
+            <p>Los usos concretos que hacemos de estas tecnologías se describen en el presente documento.</p>    
+                
+            <h2>¿Para qué se utilizan las cookies en esta web?</h2>
+            <p>Las cookies son una parte esencial de cómo funciona el Sitio Web. El objetivo principal de nuestras cookies es mejorar su experiencia en la navegación. Por ejemplo, para recordar sus preferencias (idioma, país, etc.) durante la navegación y en futuras visitas. La información recogida en las cookies nos permite además mejorar la web, adaptarla a sus intereses como usuario, acelerar las búsquedas que realice, etc..</p>
+            <p>En determinados casos, si hemos obtenido su previo consentimiento informado, podremos utilizar cookies para otros usos, como por ejemplo para obtener información que nos permita mostrarle publicidad basada en el análisis de sus hábitos de navegación.</p>
+
+            <h2>¿Para qué NO se utilizan las cookies en esta web?</h2>
+            <p>En las cookies que utilizamos no se almacena información sensible de identificación personal como su nombre, dirección, tu contraseña, etc...</p>
+
+            <h2>¿Quién utiliza la información almacenada en las cookies?</h2>
+            <p>La información almacenada en las cookies de nuestro Sitio Web es utilizada exclusivamente por nosotros, a excepción de aquellas identificadas más adelante como "cookie de terceros", que son utilizadas y gestionadas por entidades externas que nos proporcionan servicios que mejoran la experiencia del usuario. Por ejemplo las estadísticas que se recogen sobre el número de visitas, el contenido que más gusta, etc...</p>
+
+            <h2>¿Cómo puede evitar el uso de cookies en este Sitio Web?</h2>
+            <p>Si prefiere evitar el uso de las cookies, puede RECHAZAR su uso o puede CONFIGURAR las que quiere evitar y las que permite utilizar (en este documento le damos información ampliada al respecto de cada tipo de cookie, su finalidad, destinatario, temporalidad, etc... ).</p>
+            <p>Si las ha aceptado, no volveremos a preguntarle a menos que borre las cookies en su dispositivo según se indica en el apartado siguiente. Si quiere revocar el consentimiento tendrá que eliminar las cookies y volver a configurarlas.</p>
+            
+            <h2>¿Cómo deshabilito y elimino la utilización de cookies?</h2>
+            <p>Para restringir, bloquear o borrar las cookies de este Sitio Web (y las usada por terceros) puede hacerlo, en cualquier momento, modificando la configuración de su navegador. Tenga en cuenta que esta configuración es diferente en cada navegador.</p>
+            <p>En los siguientes enlaces encontrará instrucciones para habilitar o deshabilitar las cookies en los navegadores más comunes.</p>
+                <ul>
+                    <li><a href="https://support.mozilla.org/es/kb/habilitar-y-deshabilitar-cookies-sitios-web-rastrear-preferencias?redirectslug=habilitar-y-deshabilitar-cookies-que-los-sitios-we&redirectlocale=es">Firefox</a></li>
+                    <li><a href="https://support.google.com/chrome/answer/95647?hl=es">Google Chrome</a></li>
+                    <li><a href="https://support.microsoft.com/es-es/windows/eliminar-y-administrar-cookies-168dab11-0753-043d-7c16-ede5947fc64d#ie=%22ie-10">Internet Explorer</a></li>
+                    <li><a href="https://support.microsoft.com/es-es/windows/microsoft-edge-datos-de-exploraci%C3%B3n-y-privacidad-bb8174ba-9d73-dcf2-9b4a-c582b4e640dd">Microsoft Edge</a></li>
+                    <li><a href="https://support.apple.com/es-es/HT201265">Safari</a></li>
+                </ul>
+
+            <h2>¿Qué tipos de cookies se utilizan en esta página web?</h2>
+            <p>Cada página web utiliza sus propias cookies. En nuestra web utilizamos las que se indican a continuación:</p>
+            <h3>SEGÚN LA ENTIDAD QUE LO GESTIONA</h3>
+                <ul>
+                    <li><strong>Cookies propias:</strong> Son aquellas que se envían al equipo terminal del Usuario desde un equipo o dominio gestionado por el propio editor y desde el que se presta el servicio solicitado por el Usuario.</li>
+                    <li><strong>Cookies de terceros:</strong> Son aquellas que se envían al equipo terminal del Usuario desde un equipo o dominio que no es gestionado por el editor, sino por otra entidad que trata los datos obtenidos través de las cookies.</li>
+                </ul>
+                <p>En el caso de que las cookies sean servidas desde un equipo o dominio gestionado por el propio editor, pero la información que se recoja mediante estas sea gestionada por un tercero, no pueden ser consideradas como cookies propias si el tercero las utiliza para sus propias finalidades (por ejemplo, la mejora de los servicios que presta o la prestación de servicios de carácter publicitario a favor de otras entidades).</p>
+
+            <h3>SEGÚN SU FINALIDAD</h3>
+                <ul>
+                    <li><strong>Cookies técnicas:</strong> Son aquellas necesarias para la navegación y el buen funcionamiento de nuestro Sitio Web, como por ejemplo, controlar el tráfico y la comunicación de datos, identificar la sesión, acceder a partes de acceso restringido, realizar la solicitud de inscripción o participación en un evento, contar visitas a efectos de la facturación de licencias del software con el que funciona el servicio del Sitio Web, utilizar elementos de seguridad durante la navegación, almacenar contenidos para la difusión de vídeos o sonido, habilitar contenidos dinámicos (por ejemplo, animación de carga de un texto o imagen).</li>
+                    <li><strong>Cookies de análisis:</strong> Permiten cuantificar el número de usuarios y así realizar la medición y análisis estadístico de la utilización que hacen los usuarios del Sitio Web.</li>
+                    <li><strong>Cookies de preferencias o personalización:</strong> Son aquellas que permiten recordar información para que el Usuario acceda al servicio con determinadas características que pueden diferenciar su experiencia de la de otros usuarios, como, por ejemplo, el idioma, el número de resultados a mostrar cuando el Usuario realiza una búsqueda, el aspecto o contenido del servicio en función del tipo de navegador a través del cual el Usuario accede al servicio o de la región desde la que accede al servicio, etc.</li>
+                </ul>
+
+            <h3>SEGÚN EL PLAZO DE TIEMPO QUE PERMANECEN ACTIVADAS</h3>
+                <ul>
+                    <li><strong>Cookies de sesión:</strong> Son aquellas diseñadas para recabar y almacenar datos mientras el Usuario accede a una página web. Se suelen emplear para almacenar información que solo interesa conservar para la prestación del servicio solicitado por el Usuario en una sola ocasión (por ejemplo, una lista de productos adquiridos) y desaparecen al terminar la sesión.</li>
+                    <li><strong>Cookies persistentes:</strong> Son aquellas en las que los datos siguen almacenados en el terminal y pueden ser accedidos y tratados durante un periodo definido por el responsable de la cookie, y que puede ir de unos minutos a varios años. A este respecto debe valorarse específicamente si es necesaria la utilización de cookies persistentes, puesto que los riesgos para la privacidad podrían reducirse mediante la utilización de cookies de sesión. En todo caso, cuando se instalen cookies persistentes, se recomienda reducir al mínimo necesario su duración temporal atendiendo a la finalidad de su uso. A estos efectos, el Dictamen 4/2012 del GT29 indicó que para que una cookie pueda estar exenta del deber de consentimiento informado, su caducidad debe estar relacionada con su finalidad. Debido a ello, es mucho más probable que se consideren como exceptuadas las cookies de sesión que las persistentes.</li>
+                </ul>
+
+        </div>
         </>
     );
 };
--- src/pages/Privacy.jsx
+++ src/pages/Privacy.jsx
@@ -1,8 +1,123 @@
+import { Link } from 'react-router-dom';
+import "../style/Legal.css"
+import "../index.css"
+
 const Privacy = () => {
 
     return (
         <>
+           <div className="legal">
+            <h1>Política de Privacidad</h1>
+
+            <p>El Titular le informa sobre su Política de Privacidad respecto del tratamiento y protección de los datos de carácter personal de los usuarios que puedan ser recabados durante la navegación a través del Sitio Web: https://wadokan.es</p>
+            <p>En este sentido, el Titular garantiza el cumplimiento de la normativa vigente en materia de protección de datos personales, reflejada en la Ley Orgánica 3/2018, de 5 de diciembre, de Protección de Datos Personales y de Garantía de Derechos Digitales (LOPD GDD). Cumple también con el Reglamento (UE) 2016/679 del Parlamento Europeo y del Consejo de 27 de abril de 2016 relativo a la protección de las personas físicas (RGPD).</p>   
+            <p>El uso de sitio Web implica la aceptación de esta Política de Privacidad así como las condiciones incluidas en el  <Link to="/aviso-legal">Aviso Legal</Link>.</p>    
+                
+            <h2>Identidad del Responsable</h2>    
+                <ul>
+                    <li><strong>Responsable:</strong>  ..........</li>
+                    <li><strong>NIF:</strong> 00000000A</li>
+                    <li><strong>Domicilio:</strong>  Calle .... Sevilla - España</li>
+                    <li><strong>Correo electrónico:</strong>  info@wadokan.es</li>
+                    <li><strong>Teléfono de contacto:</strong>  666666666</li>
+                    <li><strong>Sitio Web:</strong>  https://wadokan.es</li>
+                </ul>
+
+            <h2>Principios aplicados en el tratamiento de datos</h2>
+            <p>En el tratamiento de sus datos personales, el Titular aplicará los siguientes principios que se ajustan a las exigencias del nuevo reglamento europeo de protección de datos (RGPD):</p>
+                <ul>
+                    <li><strong>Principio de licitud, lealtad y transparencia:</strong> El Titular siempre requerirá el consentimiento para el tratamiento de los datos personales que puede ser para uno o varios fines específicos sobre los que el Titular informará al Usuario previamente con absoluta transparencia.</li>
+                    <li><strong>Principio de minimización de datos:</strong> El Titular solicitará solo los datos estrictamente necesarios para el fin o los fines que los solicita.</li>
+                    <li><strong>Principio de limitación del plazo de conservación:</strong> El Titular mantendrá los datos personales recabados durante el tiempo estrictamente necesario para el fin o los fines del tratamiento. El Titular informará al Usuario del plazo de conservación correspondiente según la finalidad. En el caso de suscripciones, el Titular revisará periódicamente las listas y eliminará aquellos registros inactivos durante un tiempo considerable.</li>
+                    <li><strong>Principio de integridad y confidencialidad:</strong> Los datos personales recabados serán tratados de tal manera que su seguridad, confidencialidad e integridad está garantizada. El Titular toma las precauciones necesarias para evitar el acceso no autorizado o uso indebido de los datos de sus usuarios por parte de terceros.</li>
+                </ul>
+            
+            <h2>Obtención de datos personales</h2>
+            <p>Para navegar por el sitio Web no es necesario que facilite ningún dato personal.</p>
+            
+            <h2>Derechos</h2>            
+            <p>El Titular le informa que sobre sus datos personales tiene derecho a:</p>
+                <ul>
+                    <li>Solicitar el acceso a los datos almacenados.</li>
+                    <li>Solicitar una rectificación o la supresión.</li>
+                    <li>Solicitar la limitación de su tratamiento.</li>
+                    <li>Oponerse al tratamiento.</li>
+                </ul>
+            
+            <p>No puede ejercitar el derecho a la portabilidad de los datos.</p>
+            <p>El ejercicio de estos derechos es personal y por tanto debe ser ejercido directamente por el interesado, solicitándolo directamente al Titular, lo que significa que cualquier cliente, suscriptor o colaborador que haya facilitado sus datos en algún momento, puede dirigirse al Titular y pedir información sobre los datos que tiene almacenados y cómo los ha obtenido, solicitar la rectificación de los mismos, oponerse al tratamiento, limitar su uso o solicitar la supresión de esos datos en los ficheros del Titular.</p>
+            <p>Para ejercitar sus derechos tiene que enviar su petición junto con una fotocopia del Documento Nacional de Identidad o equivalente a  la dirección de correo electrónico: info@wadokan.es</p>
+            <p>El ejercicio de estos derechos no incluye ningún dato que el Titular esté obligado a conservar con fines administrativos, legales o de seguridad.</p>
+            <p>Tiene derecho a la tutela judicial efectiva y a presentar una reclamación ante la autoridad de control, en este caso, la Agencia Española de Protección de Datos, si considera que el tratamiento de datos personales que le conciernen infringe el Reglamento.</p>
+
+            <h2>Finalidad del tratamiento de datos personales</h2>
+            <p>Cuando usted se conecta al Sitio Web para mandar un correo al Titular, se suscribe a su boletín está facilitando información de carácter personal de la que el responsable es el Titular. Esta información puede incluir datos de carácter personal como pueden ser su dirección IP, nombre y apellidos, dirección física, dirección de correo electrónico, número de teléfono, y otra información. Al facilitar esta información, da su consentimiento para que su información sea recopilada, utilizada, gestionada y almacenada por — Miguel Ángel Gómez Rodríguez — sólo como se describe en las páginas:</p>
+
+            <h2>Tratamiento de Datos Personales</h2>
+            <p>Puede consultar toda la información relativa al tratamiento de datos personales que recoge el Titular en la página de <Link to="/politica-privacidad">Política de Privacidad</Link>.</p>
+                <ul>
+                    <li><Link to="/aviso-legal">Aviso Legal</Link></li>
+                    <li><Link to="/politica-privacidad">Política de Privacidad</Link></li>
+                </ul>
+            <p>Los datos personales y la finalidad del tratamiento por parte del Titular es diferente según el sistema de captura de información.</p>
+            <p>Existen otras finalidades por las que el Titular trata datos personales:</p>
+                <ul>
+                    <li>Para garantizar el cumplimiento de las condiciones recogidas en la página de Aviso Legal y de la ley aplicable. Esto puede incluir el desarrollo de herramientas y algoritmos que ayuden al Sitio Web a garantizar la confidencialidad de los datos personales que recoge.</li>
+                    <li>Para apoyar y mejorar los servicios que ofrece este Sitio Web.</li>
+                    <li>Para analizar la navegación de los usuarios. El Titular recoge otros datos no identificativos que se obtienen mediante el uso de cookies que se descargan en el ordenador del Usuario cuando navega por el Sitio Web cuyas características y finalidad están detalladas en la página de <Link to="/politica-cookies">Política de Cookies</Link>.</li>
+                </ul>
+
+
+            <h2>Seguridad de los datos personales</h2>
+            <p>Para proteger sus datos personales, el Titular toma todas las precauciones razonables y sigue las mejores prácticas de la industria para evitar su pérdida, mal uso, acceso indebido, divulgación, alteración o destrucción de los mismos.</p>
+            <p>Sus datos podrán ser incorporados a un fichero de lista de correo, del cual el Titular es responsable de su gestión y tratamiento. La seguridad de sus datos está garantizada, ya que el Titular toma todas las medidas de seguridad necesarias y le garantiza que los datos personales sólo se usarán para las finalidades dadas.</p>
+            <p>El Titular informa al Usuario de que sus datos personales no serán cedidos a terceras organizaciones, con la salvedad de que dicha cesión de datos esté amparada en una obligación legal o cuando la prestación de un servicio implique la necesidad de una relación contractual con un encargado de tratamiento. En este último caso, solo se llevará a cabo la cesión de datos al tercero cuando el Titular disponga del consentimiento expreso del Usuario.</p>
+            <p>Sin embargo, en algunos casos se pueden realizar colaboraciones con otros profesionales, en esos casos, se requerirá consentimiento al Usuario informando sobre la identidad del colaborador y la finalidad de la colaboración. Siempre se realizará con los más estrictos estándares de seguridad.</p>
+
+            <h2>Contenido de otros sitios web</h2>
+            <p>Las páginas de este sitio Web pueden incluir contenido incrustado (por ejemplo, vídeos, imágenes, artículos, etc.). El contenido incrustado de otras web se comporta exactamente de la misma manera que si hubiera visitado la otra web.</p>
+            <p>Estos sitios Web pueden recopilar datos sobre usted, utilizar cookies, incrustar un código de seguimiento adicional de terceros, y supervisar su interacción usando este código.</p>
+
+            <h2>Política de Cookies</h2>
+            <p>Para que este sitio Web funcione correctamente necesita utilizar cookies, que es una información que se almacena en su navegador web.</p>
+            <p>Puede consultar toda la información relativa a la política de recogida y tratamiento de las cookies en la página de <Link to="/politica-cookies">Política de Cookies</Link>.</p>
+
+            <h2>Legitimación para el tratamiento de datos</h2>
+            <p>La base legal para el tratamiento de sus datos es:</p>
+                <ul>
+                    <li>El consentimiento del interesado.</li>
+                </ul>
+
+            <h2>Categorías de datos personales</h2>
+            <p>Las categorías de datos personales que trata el Titular son:</p>
+                <ul>
+                    <li>Datos identificativos.</li>
+                    <li>No se tratan categorías de datos especialmente protegidos.</li>
+                </ul>
+
+            <h2>Conservación de datos personales</h2>
+            <p>Los datos personales proporcionados al Titular se conservarán hasta que solicite su supresión.</p>
            
+            <h2>Navegación Web</h2>
+            <p>Al navegar por el Sitio Web se pueden recoger datos no identificativos, que pueden incluir, la dirección IP, geolocalización, un registro de cómo se utilizan los servicios y sitios, hábitos de navegación y otros datos que no pueden ser utilizados para identificarle.</p>
+            <p>El sitio Web utiliza los siguientes servicios de análisis de terceros:</p>
+            <p>El Titular utiliza la información obtenida para obtener datos estadísticos, analizar tendencias, administrar el sitio, estudiar patrones de navegación y para recopilar información demográfica.</p>
+            <p>El Titular no se hace responsable del tratamiento de los datos personales que realicen las páginas web a las que pueda acceder a través de los distintos enlaces que contiene el Sitio Web.</p>
+
+            <h2>Exactitud y veracidad de los datos personales</h2>
+            <p>Usted se compromete a que los datos facilitados al Titular sean correctos, completos, exactos y vigentes, así como a mantenerlos debidamente actualizados.</p>
+            <p>Como Usuario del Sitio Web es el único responsable de la veracidad y corrección de los datos remitidos al Sitio Web exonerando al Titular de cualquier responsabilidad al respecto.</p>
+
+            <h2>Aceptación y consentimiento</h2>
+            <p>Como Usuario del Sitio Web declara haber sido informado de las condiciones sobre protección de datos de carácter personal, acepta y consiente el tratamiento de los mismos por parte del Titular en la forma y para las finalidades indicadas en esta Política de Privacidad.</p>
+            <p>Para contactar con el Titular, suscribirse a un boletín o realizar comentarios en este sitio Web tiene que aceptar la presente Política de Privacidad.</p>
+
+            <h2>Cambios en la Política de Privacidad</h2>
+            <p>El Titular se reserva el derecho a modificar la presente Política de Privacidad para adaptarla a novedades legislativas o jurisprudenciales, así como a prácticas de la industria.</p>
+            <p>Estas políticas estarán vigentes hasta que sean modificadas por otras debidamente publicadas.</p>
+
+
+        </div>
         </>
     );
 };
