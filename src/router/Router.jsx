e79dcca58dcb642abbfe931fd5109fabe5b803de
MariaGF <miriangimenez@gmail.com>
2023-04-11 12:19:50 +0200

    added initial changes

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -22,7 +22,7 @@ export const router = createBrowserRouter([
         children: [
                     {
                         index: true,
-                        element: <Home />,
+                        element: <Home/>,
                         //loader: fetchPhotos,
                         //loader: fetchSchedules,
                     },
