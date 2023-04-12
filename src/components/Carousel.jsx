0cc302abe96b48c7fa261d8e8a2d13e98493b0b3
MariaGF <miriangimenez@gmail.com>
2023-04-12 12:03:20 +0200

    added style Carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -49,20 +49,21 @@ function Carousel() {
   };
 
   return (
-    <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group'>
+    <div className=' max-w-[1400px] h-[780px] w-full m-0 relative group'>
       <div
         style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-        className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
-      ></div>
+        className='w-full h-[45%] bg-center bg-cover duration-300' >
+
+      </div>
       {/* Left Arrow */}
-      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-        <BsChevronCompactLeft onClick={prevSlide} size={30} />
+      <div className='hidden group-hover:block absolute top-[23%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+        <BsChevronCompactLeft onClick={prevSlide} size={20} />
       </div>
       {/* Right Arrow */}
-      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-        <BsChevronCompactRight onClick={nextSlide} size={30} />
+      <div className='hidden group-hover:block absolute top-[23%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+        <BsChevronCompactRight onClick={nextSlide} size={20} />
       </div>
-      <div className='flex top-4 justify-center py-2'>
+      <div className='flex top-4 justify-center'>
         {slides.map((slide, slideIndex) => (
           <div
             key={slideIndex}
