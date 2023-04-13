64dbcac72498cb42b1716153a1bd2190befdbc2b
MartaMunMol <marmunmol@gmail.com>
2023-04-13 10:34:00 +0200

    stylized banners

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,12 +1,14 @@
 import Cardblog from "../components/Cardblog";
+import '../style/Blog.css'
 
 const Blog = () => {
 
     return (
         <>
-        <div id="banner">RECURSOS</div>
+        <div id="banner">
+            <h1 className="h1Blog">RECURSOS</h1></div>
             <Cardblog/>
-        <div id="banner">BLOG</div>
+        <div id="banner" ><h1 className="h1Blog">NOTICIAS</h1></div>
             <Cardblog/>
         </>
     );
