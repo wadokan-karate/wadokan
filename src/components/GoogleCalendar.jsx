2c299bd17edd74259123ed15258ccd686ea6954f
MartaMunMol <marmunmol@gmail.com>
2023-04-13 14:25:44 +0200

    fixed a component mistake

--- src/components/GoogleCalendar.jsx
+++ src/components/GoogleCalendar.jsx
@@ -1,42 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { google } from 'googleapis';
+import '../style/Calendar.css'
 import "../index.css"
 
 const GoogleCalendar = () => {
-    const [events, setEvents] = useState([]);
-  
-    useEffect(() => {
-      const getEvents = async () => {
-        const auth = new google.auth.GoogleAuth({
-          // Aquí debes agregar las credenciales de Google API
-        });
-  
-        const calendar = google.calendar({ version: 'v3', auth });
-        const response = await calendar.events.list({
-          calendarId: 'primary',
-          timeMin: new Date().toISOString(),
-          maxResults: 10,
-          singleEvents: true,
-          orderBy: 'startTime',
-        });
-  
-        setEvents(response.data.items);
-      };
-  
-      getEvents();
-    }, []);
   
     return (
-      <div>
-        <h2>Calendario de Google</h2>
-        {events.map((event) => (
-          <div key={event.id}>
-            <h4>{event.summary}</h4>
-            <p>{event.start.dateTime}</p>
-            <p>{event.end.dateTime}</p>
-          </div>
-        ))}
-      </div>
+      <iframe id='iframe' src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=Europe%2FMadrid&src=NmY1Y2Y5Y2ZkOGQ4ZGUxZDIwM2QzYTcyYTFlYmQwNGIzY2IwMTEyZTM4YmU1YzE0MWNlMGEzZWFhOWVkNmZiOEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%237CB342" style={{borderwidth:0, width:800, height:600 }}
+            ></iframe>
     );
   };
   
--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,3 +1,4 @@
+import GoogleCalendar from '../components/GoogleCalendar';
 import '../style/Calendar.css'
 
 const Calendar = () => {
@@ -6,10 +7,9 @@ const Calendar = () => {
         <>
            <h1 className="h1Calendar">HORARIO</h1>
            <div className='scheduleContainer bg-wadokanBlue'>ESTO ES UN HORARIO</div>
+            <GoogleCalendar/>
 
-
-            <iframe id='iframe' src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=Europe%2FMadrid&src=NmY1Y2Y5Y2ZkOGQ4ZGUxZDIwM2QzYTcyYTFlYmQwNGIzY2IwMTEyZTM4YmU1YzE0MWNlMGEzZWFhOWVkNmZiOEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%237CB342" style={{borderwidth:0, width:800, height:600 }}
-            ></iframe>
+            
             
             
            
