b4cc229ad863b7619a645e25d9fd19aa171dccba
MartaMunMol <marmunmol@gmail.com>
2023-04-13 14:22:24 +0200

    working in the schedule component

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -5,7 +5,7 @@ const Calendar = () => {
     return (
         <>
            <h1 className="h1Calendar">HORARIO</h1>
-           <div>ESTO ES UN HORARIO</div>
+           <div className='scheduleContainer bg-wadokanBlue'>ESTO ES UN HORARIO</div>
 
 
             <iframe id='iframe' src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=Europe%2FMadrid&src=NmY1Y2Y5Y2ZkOGQ4ZGUxZDIwM2QzYTcyYTFlYmQwNGIzY2IwMTEyZTM4YmU1YzE0MWNlMGEzZWFhOWVkNmZiOEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%237CB342" style={{borderwidth:0, width:800, height:600 }}
