a18bc2fabb89c5a4946da45b0ac0f73bacaddf71
Milacover <117834108+Milacover@users.noreply.github.com>
2023-04-17 12:49:56 +0200

    change Service conexion with autorization

--- src/service/eventService.js
+++ src/service/eventService.js
@@ -1,14 +1,21 @@
 import axios from "axios";
 
+// const apiClient = axios.create({
+//     baseURL: 'https://localhost:7287',
+//     withCredentials: false,
+//     method: 'post',
+//     headers: {
+//         // 'Content-Type': 'text/plain;charset=utf-8',
+//         'Content-Type': 'application/json',
+//         'Authorization': 'veronicasm79@hotmail.com:VBiWA/Ll3mGqRDkA90J9Bllp/N7FMF5z0yuYN+GRI7zsx3immL1dTL2xXoqt8otVDAoWmHbpSaErhQGc+A/ntw==',
+//         Accept: 'application/json'
+//     }
+// })
+
 const apiClient = axios.create({
-    baseURL: 'https://localhost:7287',
-    withCredentials: false,
-    headers: {
-        'Content-Type': 'application/json',
-       // 'Authorization': 'veronicasm79@hotmail.com:VBiWA/Ll3mGqRDkA90J9Bllp/N7FMF5z0yuYN+GRI7zsx3immL1dTL2xXoqt8otVDAoWmHbpSaErhQGc+A/ntw==',
-        Accept: 'application/json'
-    }
-})
+    baseURL: 'https://localhost:7287'
+});
+
 export const eventService = {
 
     async getProductsBase64Array() {
@@ -21,14 +28,38 @@ export const eventService = {
     },
 
     async submitProductImgBase64(newProductBase64RequestModel) {
-        let response = await apiClient.post("/Event/PostBase64", newProductBase64RequestModel)
+        let config = {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'veronicasm79@hotmail.com:VBiWA/Ll3mGqRDkA90J9Bllp/N7FMF5z0yuYN+GRI7zsx3immL1dTL2xXoqt8otVDAoWmHbpSaErhQGc+A/ntw==',
+                Accept: 'application/json'
+            }
+        }
+        let response = await apiClient.post("/Event/PostBase64", newProductBase64RequestModel, config);
         if (response.status == 200) {
             alert("Producto insertado correctamente con id " + response.data);
         } else {
             alert("Upsi...hubo un error al insertar el evento");
         }
     },
-   /* async deleteProduct(id){
-                await apiClient.delete("/Event/DeleteById? + id)
-             },*/
+
+    // async submitProductImgBase64(newProductBase64RequestModel) {
+    //     let response = await apiClient.post('/Event/PostBase64', {
+    //         // method: 'post',
+    //         // url: '/Event/PostBase64',
+    //         data: newProductBase64RequestModel,
+    //         headers: {
+    //             'Content-Type': 'text/plain;charset=utf-8',
+    //             'Authorization': 'veronicasm79@hotmail.com:VBiWA/Ll3mGqRDkA90J9Bllp/N7FMF5z0yuYN+GRI7zsx3immL1dTL2xXoqt8otVDAoWmHbpSaErhQGc+A/ntw=='
+    //         }
+    //     });
+    //     if (response.status == 200) {
+    //         alert("Producto insertado correctamente con id " + response.data);
+    //     } else {
+    //         alert("Upsi...hubo un error al insertar el evento");
+    //     }
+    // }
 }
+
+   
