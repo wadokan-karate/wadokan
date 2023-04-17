7eaa3d8a1c296be798db94f15f58de416d760b02
MariaGF <miriangimenez@gmail.com>
2023-04-18 01:18:55 +0200

    added new images to Carousels and modified schedule style

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,10 +3,10 @@ import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
 import { RxDotFilled } from 'react-icons/rx';
 import Logo from '../assets/img/SlideHome/Foto1.png';
 import LogoName from '../assets/img/SlideHome/Foto2.png';
-import Equipation from '../assets/img/SlideHome/Foto3.png';
 import CintoBlack from '../assets/img/SlideHome/Foto5.png';
 import CintoYellow from '../assets/img/SlideHome/Foto4.png';
-// import PalaciosDojo from '../assets/img/SlideHome/PalaciosDojo.jpg';
+import LosPalaciosDojo from '../assets/img/SlideHome/LosPalaciosDojo.png';
+import ImgDojoLogo from '../assets/img/SlideHome/LogoDojo.png';
 
 
 
@@ -20,13 +20,15 @@ function Carousel() {
       url: LogoName,
     },
     {
-      url: Equipation,
+      url: LosPalaciosDojo,
     },
 
     {
-        url: Logo,
+      url: ImgDojoLogo,
     },
-    
+    {
+        url: Logo,
+    },    
     {
         url: CintoYellow,
     },
@@ -51,7 +53,7 @@ function Carousel() {
   };
 
   return (
-    <div className=' CarouselBox max-w-[1400px] h-[480px] w-full m-0 '>
+    <div className=' CarouselBox max-w-[1400px] h-[480px] w-full m-0 mb-3 '>
       
       <div
         style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
