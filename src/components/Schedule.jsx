c7556548133f0159d7980cb4eaf04e41116f4e09
Carmen-Trillo <mctrillon@gmail.com>
2023-04-17 09:52:41 +0200

    Improved Shedule Components

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -1,25 +1,70 @@
 import React, { useState, useEffect } from 'react';
 import scheduleHandler from '../handlers/scheduleHandler';
+import "../style/Schedule.css"
+import "../index.css"
+
 
 export default function Schedule() {
   const [schedules, setSchedules] = useState([]);
   const [city, setCity] = useState('');
   const [location, setLocation] = useState('');
+  const [citiesList, setCitiesList] = useState([]);
+  const [locationsList, setLocationsList] = useState([]);
+
+  useEffect(() => {
+    async function fetchInitialData() {
+      const data = await scheduleHandler.loadSchedules();
+      setSchedules(data);
+      setCitiesList([...new Set(data.map(schedule => schedule.city))]);
+
+      const filteredData = data.filter(schedule => schedule.city === 'Montequinto');
+      setSchedules(filteredData);
+
+      const firstLocation = filteredData[0]?.location;
+      if (firstLocation) {
+        setCity('Montequinto');
+        setLocation(firstLocation);
+      }
+    }
+
+    fetchInitialData();
+  }, []);
 
   useEffect(() => {
     fetchSchedule(city, location);
   }, [city, location]);
 
+  useEffect(() => {
+    if (!city && location) {
+      // Filtrar las ciudades por ubicación
+      const citiesFiltered = [...new Set(schedules.filter(schedule => schedule.location === location).map(schedule => schedule.city))];
+      if (citiesFiltered.length > 0) {
+        setCity(citiesFiltered[0]);
+      }
+    }
+  }, [location, city, schedules]);
+
   async function fetchSchedule(city, location) {
     try {
       const data = await scheduleHandler.loadSchedules();
-      const filteredData = data.filter((schedule) => {
+      let filteredData = data.filter((schedule) => {
         return (
-          (!city || schedule.city === city) && 
+          (!city || schedule.city === city) &&
           (!location || schedule.location === location)
         );
       });
+
       setSchedules(filteredData);
+
+      // Filtrar las ubicaciones por ciudad
+      let locationsFiltered = [...new Set(filteredData.map(schedule => schedule.location))];
+      if (city) {
+        locationsFiltered = locationsFiltered.filter(location => {
+          return filteredData.some(schedule => schedule.city === city && schedule.location === location);
+        });
+      }
+
+      setLocationsList(locationsFiltered);
     } catch (error) {
       console.error(error);
     }
@@ -29,35 +74,45 @@ export default function Schedule() {
 
   return (
     <div>
-      <select id="city" name="city" onChange={(e) => {
-        setCity(e.target.value);
-        fetchSchedule(e.target.value, location);
+      <h2 id="schedulesTitle">Horario</h2>
+      <div id="containerSchedule">
+        <p>Puede consultar el horario de otras localidades y ubicaciones filtrando los datos a continuación</p>
+        <select id="city" name="city" value={city} onChange={(e) => {
+          if (city !== e.target.value) {
+            setCity(e.target.value);
+            setLocation('');
+          }
         }}>
-        <option value="">Selecciona otra ciudad...</option>
-        {[...new Set(schedules.map(schedule => schedule.city))].map(city => (
-        <option key={city} value={city}>{city}</option>
-        ))}
-      </select>
+          <option value="">Selecciona una localidad</option>
+          {citiesList.map(city => (
+            <option key={city} value={city}>{city}</option>
+          ))}
+        </select>
 
-      {/* <select id="location" name="location" value={location} onChange={(e) => {
-        setLocation(e.target.value);
-        fetchSchedule(city, e.target.value);
-      }}>
-        <option value="">Selecciona otra ubicación</option>
-        {[...new Set(schedules.map(schedule => schedule.location))].map(location => (
-          <option key={location} value={location}>{location}</option>
-        ))}
-      </select> */}
+        <select id="location" name="location" value={location} onChange={(e) => {
+          setLocation(e.target.value);
+        }}>
+          <option value="">Selecciona una ubicación</option>
+          {locationsList.map(location => (
+            <option key={location} value={location}>{location}</option>
+          ))}
+        </select>
 
       <table>
         <thead>
+            <tr>
+            <th id='cityTitle' colSpan="6">{city}</th>
+          </tr>
+          <tr>
+            <th id='locationTitle' colSpan="6">{location}</th>
+          </tr>
           <tr>
             <th></th>
-            <th>Lunes</th>
-            <th>Martes</th>
-            <th>Miércoles</th>
-            <th>Jueves</th>
-            <th>Viernes</th>
+            <th className='dayTitle'>Lunes</th>
+            <th className='dayTitle'>Martes</th>
+            <th className='dayTitle'>Miércoles</th>
+            <th className='dayTitle'>Jueves</th>
+            <th className='dayTitle'>Viernes</th>
           </tr>
         </thead>
         <tbody>
@@ -75,6 +130,7 @@ export default function Schedule() {
           ))}
         </tbody>
       </table>
+      </div>
     </div>
   );
 }
