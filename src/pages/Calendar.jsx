734c00f08421cdf05d44de3103c331455e70404b
MartaMunMol <marmunmol@gmail.com>
2023-04-17 14:13:57 +0200

    updating calendarview with schedule

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,17 +1,13 @@
 import GoogleCalendar from '../components/GoogleCalendar';
+import Schedule from '../components/Schedule';
 import '../style/Calendar.css'
 
 const Calendar = () => {
 
     return (
         <>
-           <h1 className="h1Calendar">HORARIO</h1>
-           <div className='scheduleContainer bg-wadokanBlue'>ESTO ES UN HORARIO</div>
+            <Schedule />
             <GoogleCalendar/>
-
-            
-            
-            
            
         </>
     );
