6db4527a43e86fb58984b91c13bf48e1c194e91a
Ma-shi22 <mashisantos@gmail.com>
2023-04-18 12:26:46 +0200

    add imageRigths

---
+++ src/components/ImageRights.jsx
@@ -0,0 +1,24 @@
+
+
+
+
+const ImageRights = () => {
+
+    return (
+    <>
+        <article>
+        <H3>Formulario de Consentimiento Para uso de Fotografías y/o Vídeos</H3>
+
+        <P>EL formulario de Consentimiento de foto/vídeo se utiliza para obtener permiso del propietario de una foto o de las personas en la foto. Al completar el formulario, los propietarios de fotografías pueden aceptar sus deseos sobre el uso de sus fotografías, imágenes o ilustraciones.</P>
+        <p>Los derechos de imagen se regulan en la Ley Orgánica 1/1982, de 5 de mayo, sobre protección civil del derecho al honor, a la intimidad personal y familiar y a la propia imagen.</p>
+        <p>Una vez cumplimentado, esta autorización debe ser firmada por el cedente (o por su representante legal, en caso de tratarse de un menor de edad).</p>
+        <h4>Pincha el enlace pare rellenar el formulário</h4> 
+            
+            <iframe id='iframe' src="" style={{ }}></iframe>
+
+        </article>
+    </>
+    );
+};
+
+export default ImageRights;
