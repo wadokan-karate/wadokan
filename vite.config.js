c2e79c5ff3cd01cf2a61e45cdad9d3563e01a1dc
MartaMunMol <marmunmol@gmail.com>
2023-04-18 13:11:57 +0200

    installed dependencies of unit test and cheking testing, testing are ready to be written

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/react-marketplace/",
   plugins: [react()],
+  test: {
+  environment: "jsdom",
+  globals: true
+  } 
 })
