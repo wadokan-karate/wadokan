1eea2aaca3650ba3fc3ee71a1dafdb89758e0130
MariaGF <miriangimenez@gmail.com>
2023-04-19 21:31:40 +0200

    added new Footer

--- src/components/HealthInsurance.jsx
+++ src/components/HealthInsurance.jsx
@@ -3,7 +3,15 @@
 const HealthInsurance = () => {
   
     return (
-      <h1 className="text-black">ESTA ES LA INFO DEL SEGURO MÉDICO</h1>
+
+      <div className=" bg-white w-full h-full">
+      <div className=" ContainerHealth bg-brown-300 text-center w-full">
+       <h1 className="text-black">ESTA ES LA INFO DEL SEGURO MÉDICO</h1>
+       <p>ljvnrievpervin
+       </p>
+      </div>
+      </div>
+
     );
   };
   
--- src/pages/LicensesHealthInsurance.jsx
+++ src/pages/LicensesHealthInsurance.jsx
@@ -1,19 +1,22 @@
 import HealthInsurance from '../components/HealthInsurance.jsx';
 import SidebarUser from '../components/SidebarUser.jsx';
 import Pruebasidebar from '../components/pruebasidebar.jsx';
+import '../components/style/HealthInsurance.css';
 import "../style/Licenses.css"
 
     const Licenses = () => {
 
         return (
             
-            <>
-                    <div className="flex"> 
-                    <Pruebasidebar></Pruebasidebar>
-                        {/* <SidebarUser /> */}
-                        <HealthInsurance/>
-                    </div>            
-                     
+            <> 
+              <div className="container-health float-left ">
+                <div className='flex justify-center'>
+                    
+                       <Pruebasidebar></Pruebasidebar>
+                        {/* <SidebarUser /> */} 
+                       <HealthInsurance/>                
+                </div>       
+              </div>
             </>
         
         )
