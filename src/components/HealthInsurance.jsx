6f9ab2507a99409a3394e615aa1de8a374fa5bb6
MartaMunMol <marmunmol@gmail.com>
2023-04-20 11:57:00 +0200

    fixed HealthInsurance img

--- src/components/HealthInsurance.jsx
+++ src/components/HealthInsurance.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import SeguroMedico from "../assets/img/seguroMedico.png";
+import '../components/style/HealthInsurance.css'
 
 
 const HealthInsurance = () => {
@@ -8,7 +9,7 @@ const HealthInsurance = () => {
 
      
       <div className=" ContainerHealth bg-brown-300 text-center w-full">
-         <img src={SeguroMedico} alt="Descripción de la imagen" />
+         <img src={SeguroMedico} className="Healthimg" alt="Descripción de la imagen" />
          
        <h1 className="text-black mt-8">SEGURO DEPORTIVO</h1>
        <h2>PROTOCOLO DE ACTUACIÓN EN CASO DE ACCIDENTES</h2>
