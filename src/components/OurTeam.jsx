d9f829228a016a796a72a030cae0e56c7aa0d1d7
MariaGF <miriangimenez@gmail.com>
2023-05-02 16:11:58 +0200

    merged from resources and modified responsive

--- src/components/OurTeam.jsx
+++ src/components/OurTeam.jsx
@@ -1,34 +1,33 @@
-import Trainers from './Trainers'
 import '../style/ourTeam.css'
 
 const OurTeam = () => {
 
     return (
         <>
-        <div className="OurTeam">
-                <div className='senseiSection'> 
-                    <h1>LAS SENSEIS</h1>
-                    <div className='photoContainer'>
-                        <div className='foto1'></div>
-                        <div className='foto2'></div>
-                    </div>
+           <div className="OurTeam">
+           <div className='senseiSection'> 
+                <h1>LAS SENSEIS</h1>
+                <div className='photoContainer'>
+                    <div className='foto1'></div>
+                    <div className='foto2'></div>
                 </div>
+           </div>
 
            <div className='teachersSection'>
             <h1>Nuestros Monitores</h1>
                 <div className='teacherPhotoContainer'>
-                    {/* <div className='teacherPhotodivider'>
-                        <div className='teachersPhoto'></div>
-                        <div className='teachersPhoto'></div>
+                    {/* <div>
+                    <div className='teachersPhoto'></div>
+                    <div className='teachersPhoto'></div>
                     </div>
-                    <div className='teacherPhotodivider'>
+                    <div>
                         <div className='teachersPhoto'></div>
-                        <div className='teachersPhoto'></div>
-                    </div> */}
-                    <Trainers />
+                    <div className='teachersPhoto'></div>
+                    </div>
+                     */}
                 </div>
            </div>
-        </div>
+            </div>
         </>
         );
     };
