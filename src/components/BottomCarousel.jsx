212ecbe6c0ebf18987ed9cd714cff0f85dc2ddda
MariaGF <miriangimenez@gmail.com>
2023-05-04 01:04:39 +0200

    commented BottomCarousel mobile and tablet view

--- src/components/BottomCarousel.jsx
+++ src/components/BottomCarousel.jsx
@@ -166,7 +166,7 @@ const BottomCarousel = () => {
                 </CarouselProvider>
 
                 {/* Carousel for tablet and medium size devices */}
-                <CarouselProvider className="lg:hidden md:block hidden" naturalSlideWidth={100} isIntrinsicHeight={true} totalSlides={12} visibleSlides={2} step={1} infinite={true}>
+                {/* <CarouselProvider className="lg:hidden md:block hidden" naturalSlideWidth={100} isIntrinsicHeight={true} totalSlides={12} visibleSlides={2} step={1} infinite={true}>
                     <div className="w-full relative flex items-center justify-center">
                         <ButtonBack role="button" aria-label="slide backward" className="absolute z-30 left-0 ml-8 focus:outline-none focus:bg-gray-400 focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 cursor-pointer" id="prev">
                             <svg width={8} height={14} viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -317,10 +317,10 @@ const BottomCarousel = () => {
                             </svg>
                         </ButtonNext>
                     </div>
-                </CarouselProvider>
+                </CarouselProvider> */}
 
                 {/* Carousel for mobile and Small size Devices */}
-                <CarouselProvider className="block md:hidden " naturalSlideWidth={100} isIntrinsicHeight={true} totalSlides={12} visibleSlides={1} step={1} infinite={true}>
+                {/* <CarouselProvider className="block md:hidden " naturalSlideWidth={100} isIntrinsicHeight={true} totalSlides={12} visibleSlides={1} step={1} infinite={true}>
                     <div className="w-full relative flex items-center justify-center">
                         <ButtonBack role="button" aria-label="slide backward" className="absolute z-30 left-0 ml-8 focus:outline-none focus:bg-gray-400 focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 cursor-pointer" id="prev">
                             <svg width={8} height={14} viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -471,7 +471,7 @@ const BottomCarousel = () => {
                             </svg>
                         </ButtonNext>
                     </div>
-                </CarouselProvider>
+                </CarouselProvider> */}
             </div>
         </div>
     
