32619ad7f876a1c7156e9f0866c3372429c9ce20
Milacover <117834108+Milacover@users.noreply.github.com>
2023-05-04 15:24:19 +0200

    History

--- src/components/ClubHistory.jsx
+++ src/components/ClubHistory.jsx
@@ -12,7 +12,39 @@ const ClubHistory = () => {
             <div id='OurHistPhoto'><img className=' rounded-md'  src="src\assets\MariaCamino.png" alt="Img de la historia de nuestro club"  /></div>
             <div className='OurHistoryText rounded-md '>
                 <h1>LA HISTORIA DE NUESTRO CLUB</h1>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam nobis quis tempora dolorem, eveniet illo. Libero laborum soluta perferendis quasi aspernatur dolores fuga velit saepe alias? Commodi aperiam vel natus voluptate saepe voluptatem laboriosam reiciendis hic, at, quis quia, provident libero pariatur molestias velit quae sequi? Assumenda nam earum, perferendis tenetur quidem ipsum quo aliquid qui, ex et delectus ad magnam, eveniet dolorum facilis! Expedita quasi nobis nihil cupiditate temporibus libero ad numquam molestiae officia laborum nulla amet nemo porro voluptatem deserunt, possimus, voluptates, aliquam inventore tempora quisquam assumenda totam beatae quaerat. Ab velit animi veniam adipisci deleniti, aut quae sunt qui. Laborum est dolorum aliquam, recusandae tenetur provident sed, culpa omnis reiciendis quaerat aliquid rerum. Inventore, nulla. Pariatur mollitia maxime corrupti magnam est aliquid inventore sit? Eum incidunt quis impedit ad, iure eveniet? Autem placeat numquam velit ducimus! Exercitationem, doloremque? Distinctio necessitatibus natus eaque eum ea veritatis, facere corrupti fugit sed blanditiis praesentium perferendis enim. Id veniam rem temporibus ipsam minus consequatur suscipit magni sapiente. Eveniet harum voluptates quis assumenda soluta, ut quisquam sint, laborum impedit, adipisci sed! Voluptatum itaque voluptatem culpa eligendi cum fugiat ab dolores quae? Quisquam iure aperiam aliquam, fugiat ratione vero illum libero officia ut.</p>
+                <br></br>
+                <p>Como breve reseña histórica hay que señalar que el club de karate Wadokan nació en el año 1989,
+donde la entonces profesora María Teresa Márquez Raya(alumna de José Narbona) propuso iniciar grupos
+de perfeccionamiento de karate además de los que ya estaban de iniciación en el patronato municipal de deportes de Dos hermanas.De esta forma también se empezó
+a impartir clases por colegios como Cantely y Carlos I hasta que en el 1991 se añadio
+además en pabellón municipal de los Montecillos, al igual que en el pabellón municipal de
+Montequinto en el año 1992.
+<br></br>
+El club Wadokan ha sido responsable de las escuelas deportivas de karate del Ayuntamiento de
+Dos Hermanas, ayudando a conocer y promover nuestro arte marcial.
+El estilo que de karate que se practicaba era shotokai desde sus inicios.
+Cristina Romero se hizo cargo del club hace 21 años cuando su maestra María Teresa le cedió las escuelas y empezó a expandirse por diversas instalaciones, siendo a partir de entonces cuando se empieza pocos años después a introducir el cambio de estilo de karate a shotokan y se afianzó con la llegada de Verónica de los Santos alumna del Maestro Jesús Coronil con el que ambas han llevado a cabo su aprendizaje en karate y en dicho estilo.
+Las escuelas deportivas de karate se centralizaron en centros deportivos pertenecientes al ayuntamiento en lugar de centros educativos de forma parecida a cómo están en la actualidad, pasando diversos profesores/as del propio club y de otros de Sevilla.
+ A lo largo de nuestra historia,
+han obtenido con nosotros el grado de Cinturón Negro muchos karatekas, pero realmente de lo que
+nos sentimos orgullosos, es de haber contribuido al desarrollo personal y la educación de muchísimos
+alunnos y alumnas. Sin embargo, esto no está reñido con el rendimiento deportivo, y contamos con varias
+medallas en Campeonatos Autonómicos y nacionales.
+<br></br>
+En este momento somos alrededor de 350 karatekas de todas las edades y niveles y contamos con
+profesores titulados por la Federación Andaluza de Karate y la Junta de Andalucía: Cristina Romero Dorado
+Verónica de los Santos Monterrubio, Gema Fernández, Manuel Heredia Delgado, María Giménez Figueroa
+y Francisco Javier Vallejo Rodríguez. Además de un futuro miembro que se encuentra titulando cómo Nathan Naranjo, María Camino, María Fernández, María Camino y Samuel Palomo.
+El club tiene como finalidad seguir gestionando las escuelas deportivas municipales y seguir
+impulsando su crecimiento como ha estado haciendo en los últimos años.
+Esta escuela de karate dinamiza actividades orientadas a la enseñanza inicial y de perfeccionamiento,
+con la ayuda de los monitores/as especializados.<br></br>
+Se exponen los diversos contenidos y objetivos a conseguir durante el periodo estipulado. Así como
+apartados de competición, dinamización y división por niveles.
+EXPERIENCIA EN LA PRESTACIÓN EN SERVICIOS SIMILARES
+Los profesores/as del club deportivo Wadokan cuentan con bastante experiencia tanto en el
+ámbito deportivo como en la propia modalidad que aquí presentamos. </p>
+
             </div>
         </div>   
         </>
